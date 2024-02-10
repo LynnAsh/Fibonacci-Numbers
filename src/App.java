034eaf5ac4c36b0591ca33b83386e44a2a171dc9
LynnAsh <emdog112@gmail.com>
2024-02-10 14:31:51 -0500

    initial

---
+++ src/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        if(args.length > 0){
+            try{
+                int usrNum = Integer.parseInt(args[0]);
+                if(usrNum > 0){
+                    long f1=0,f2=1,f3=0;
+                    for(int i=0;i<usrNum-2;i++){
+                        f3 = f1 + f2;
+                        f1 = f2;
+                        f2 = f3;
+                    }
+                    System.out.println("Fibanacci number " + usrNum + " = " + f3);
+                }else{
+                    System.out.println("Number must be positive.");
+                }
+                
+            }catch(Exception e){
+                System.out.println("Error in reading argument.");
+            }
+
+        }else{
+            System.out.println("No number entered.");
+        }
+    }
+}
